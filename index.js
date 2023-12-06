7b95d3e9559d4f09f06efc8fc117f8174b7520be
Amr Elsehety <amr.abdelazeem22@gmail.com>
2023-12-06 19:58:12 +0300

    Create functionality for the toggle button

---
+++ index.js
@@ -0,0 +1,11 @@
+const toggleButton = $(".duration-button");
+toggleButton.on("click", ()=>{
+    toggleButton.toggleClass("node-start");
+    if(toggleButton.hasClass("node-start")){
+        $(".monthly-price").hide();
+        $(".annualy-price").show();
+    }else{
+        $(".monthly-price").show();
+        $(".annualy-price").hide();
+    }
+});
